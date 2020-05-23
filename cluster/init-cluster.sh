9cfb23c428601c4c93abc73b0642d31104b04518
Rafael Calleja <rafaelcalleja@gmail.com>
2020-05-23 09:27:00 +0200

    dinamyc prefix

--- cluster/init-cluster.sh
+++ cluster/init-cluster.sh
@@ -71,6 +71,8 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
+IP_PREFIX=$(docker inspect --format='{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge|grep -oP '(?:\d{1,3}\.){1}\d{1,3}'|head -n1)
+
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: ConfigMap
@@ -83,7 +85,7 @@ data:
     - name: default
       protocol: layer2
       addresses:
-      - 172.21.255.1-172.21.255.250
+      - $IP_PREFIX.255.1-$IP_PREFIX.255.250
 EOF
 
 cat <<EOF | kubectl apply -f -
