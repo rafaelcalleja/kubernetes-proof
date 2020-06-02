5fd0d001839ff83d1db7bec367e375aa49685cec
Rafael Calleja <rafaelcalleja@gmail.com>
2020-06-02 18:16:10 +0200

    remove load balancer

--- helm/install-jenkins.sh
+++ helm/install-jenkins.sh
@@ -13,9 +13,13 @@ helm install cd-jenkins stable/jenkins -f jenkins/values.yaml --wait
 ### password
 printf $(kubectl get secret --namespace default cd-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo
 
-### Host to connect
-export SERVICE_IP=$(kubectl get svc --namespace default cd-jenkins --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
-echo http://$SERVICE_IP:8080/login
+### Host to connect // LoadBalancer Version
+#export SERVICE_IP=$(kubectl get svc --namespace default cd-jenkins --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
+#echo http://$SERVICE_IP:8080/login
+
+export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/component=jenkins-master" -l "app.kubernetes.io/instance=cd-jenkins" -o jsonpath="{.items[0].metadata.name}")
+echo http://127.0.0.1:8080
+kubectl --namespace default port-forward $POD_NAME 8080:8080
 
 #helm uninstall cd-jenkins -n default
 #kubectl delete deployment -n default cd-jenkins
