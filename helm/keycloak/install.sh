20dcc485f66a98035e638ee84ac8b3ecc546337f
Rafael Calleja <rafaelcalleja@gmail.com>
2020-12-05 11:23:49 +0200

    update cfg

--- helm/keycloak/install.sh
+++ helm/keycloak/install.sh
@@ -2,9 +2,8 @@
 
 #https://github.com/codecentric/helm-charts/tree/master/charts/keycloak
 helm repo add codecentric https://codecentric.github.io/helm-charts
-helm install keycloak codecentric/keycloak
+helm upgrade keycloak codecentric/keycloak --install --values myValues.yaml
 
 #export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=keycloak,app.kubernetes.io/instance=keycloak" -o name)
 #echo "Visit http://127.0.0.1:8080 to use your application"
 #kubectl --namespace default port-forward "$POD_NAME" 8080
-
