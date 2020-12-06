9d2cf11fcbfd0e347631e96021f5209e8d0c4cd8
Rafael Calleja <rafaelcalleja@gmail.com>
2020-12-06 08:49:45 +0200

    update

--- helm/octant-dashboard/install.sh
+++ helm/octant-dashboard/install.sh
@@ -3,3 +3,7 @@
 #https://github.com/aleveille/octant-dashboard-turnkey
 helm repo add octant-dashboard https://aleveille.github.io/octant-dashboard-turnkey/repo
 helm upgrade octant-dashboard octant-dashboard/octant --namespace octant  --install --values myValues.yaml
+
+#https://stackoverflow.com/questions/53550321/keycloak-gatekeeper-aud-claim-and-client-id-do-not-match
+#https://github.com/louketo/louketo-proxy/issues/529
+
